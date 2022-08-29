5583fb9e6ad18cf049734cced349fd580df996df
mdshadhinkn <mdshadhinkn@gmail.com>
2022-08-30 03:55:23 +0600

    prejoin update

--- prejoin.py
+++ prejoin.py
@@ -9,12 +9,12 @@ def convDi(value, digit=2):
 
 apre = input("Enter audio pre: ")
 rag = int(input("Enter Range: ")) 
-process = Popen(["ffmpeg","-f", "mp3", "-i", "-", "-c:a", "copy", "arabn.mp3"], stdin=subprocess.PIPE)
+suraid = int(input("Enter Sura Id: ")) 
+process = Popen(["ffmpeg","-f", "mp3", "-i", "-", "sura-"+str(suraid)+".mp3"], stdin=subprocess.PIPE)
 fin = process.stdin
 for i in range(1, rag+1):
-    with Popen(["ffmpeg","-f", "mp3", "-i", "https://download.quranicaudio.com/verses/Alafasy/mp3/"+convDi(2,3)+convDi(i,3)+".mp3", "-f", "mp3","-c:a", "copy", "-"], stdout=subprocess.PIPE) as audio:
-        fin.write(audio.stdout.read())
     with open(apre+str(i)+".mp3", "rb") as audio:
+        quran = Popen(["ffmpeg","-f", "mp3", "-i", "https://download.quranicaudio.com/verses/Alafasy/mp3/"+convDi(suraid,3)+convDi(i,3)+".mp3", "-f", "mp3","-c:a", "copy", "-"], stdout=subprocess.PIPE)
+        fin.write(quran.stdout.read())
         fin.write(audio.read())
-fin.close()
-process.terminate()
+fin.flush()
